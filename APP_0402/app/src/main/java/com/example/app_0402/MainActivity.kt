703381100d9e97b002fb1f8f58f1ca9836c60135
RayChen0513 <chenrayray20@gmail.com>
2025-04-09 10:34:34 +0800

    04/09

--- APP_0402/app/src/main/java/com/example/app_0402/MainActivity.kt
+++ APP_0402/app/src/main/java/com/example/app_0402/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.app_0402
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +20,14 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val edText = findViewById<EditText>(R.id.edText)
+        val text = findViewById<TextView>(R.id.text)
+        val group = findViewById<RadioGroup>(R.id.group)
+        val submit = findViewById<Button>(R.id.submit)
+        val text1 = findViewById<TextView>(R.id.text1)
+        val text2 = findViewById<TextView>(R.id.text2)
+        val text3 = findViewById<TextView>(R.id.text3)
+        val text4 = findViewById<TextView>(R.id.text4)
     }
 }
